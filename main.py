8168de5a845a0b037977a8f71445aa497db29170
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2023-03-27 21:30:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# lot = 3
+
+# class Pelican:
+#   def __init__(self,wings,legs,neck,eyes,beak,lot):
+#     self.wings = wings
+#     self.legs = legs
+#     self.neck = neck
+#     self.eyes = eyes
+#     self.beak = beak
+#     self.lot = lot
+#   wings = 4
+#   legs = 2
+#   neck = 1
+#   eyes = 2
+#   beak = 1
+  
+#   def fly(self, wings):
+#     return (f'Bird can fly with {wings} wings.')
+
+#   def walk(self, legs):
+#     return (f'Bird can walk with {legs} legs.')
+
+#   def swim(self):
+#     return (f'Bird can swim.') 
+
+
+# wallie = Pelican(2,2,1,2,1,lot)
+# print(wallie.lot())
